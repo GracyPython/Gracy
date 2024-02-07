ca08a2adaa1eaa671a6574966abb25bcebea1df5
alixaprodev <haxratali0@gmail.com>
2024-02-07 23:13:33 +0500

    this was our first class

---
+++ run.py
@@ -0,0 +1,91 @@
+# printing a string
+# print('hello world')
+
+# Data Types in Python
+# 1. Numbers 
+# 1.1 integers 
+# 1.2 float
+
+# 2. String
+# 'string', "string", """multiline string"""
+
+# 3. Lists 
+# 4. Sets
+# 5. Tuples
+# 6. None 
+# 7. dictionries
+# 8. Boolean
+
+# WHat is the diff  between list vs tuple
+number = [1,3,5]
+tuple = (1,3,5)
+
+contry={
+    'india':'deli',
+    'pakistan':'isb',
+    'nepal':'deli',
+}
+# cap = contry.get('india')
+# print(cap)
+
+# What are the 2 methods to get value of a dict
+# cap_1 = contry['india']
+# print(cap_1)
+
+
+# List  length
+# length = len(number)
+# print(length)
+
+# indexes
+ist_el = number[0]
+# print(ist_el)
+
+# backward indexing is also possible in list
+
+# function on dict
+empyt_dict = dict()
+empty_dict_1= {}
+
+# order in diectonries
+# print(contry)
+
+# get the list of all the keys and values
+keys = contry.keys()
+# print(keys)
+
+
+values = contry.values()
+# print(values)
+
+# types casting 
+# conversion of types
+
+my_number = 4
+my_number_str = '4'
+my_number_flot = 4.0
+
+
+# Use int( ) function to convert other compatible types to integer
+int_str = int(my_number_str)
+print(type(int_str))
+
+# use str() to convert other types to string
+string_number = str(my_number)
+
+# use float for float conversion
+
+
+# indentifiers naming convetion in python
+# variables
+# should start with a _, alphabits not with number and special characters 
+# 
+
+my_variable = 5
+
+
+
+
+
+# merging two string
+
